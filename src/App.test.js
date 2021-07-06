369c86900661cb123071b5e272aaf337dec623f2
nikhilgoud <nikhilgoud24@gmail.com>
2021-07-06 11:49:13 +0530

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders User Management', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/User Management/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import {FC} from 'react';
+import './App.css';
+
+const App: FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">User Management</header>
+    </div>
+  );
+};
+
+export default App;
