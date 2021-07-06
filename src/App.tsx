537cd2b4126b5119e7c2d4dcae2322f7cd132849
nikhilgoud <nikhilgoud24@gmail.com>
2021-07-06 20:59:07 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import {FC} from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import './App.css';
 
 const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">User Management</header>
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <div className="content">Container</div>
+      </div>
+      <Footer />
+    </>
   );
 };
 
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import {FC} from 'react';
+
+const Footer: FC = () => {
+  return <footer>Copyright Footer</footer>;
+};
+
+export default Footer;
