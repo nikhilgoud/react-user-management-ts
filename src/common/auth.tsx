16ff6f85a77e42e0dbcd3a9964986eb472ad9998
Nikhil Goud <nikhilgoud24@gmail.com>
2021-07-22 14:54:56 +0530

    Revert "Task 1"

--- src/common/auth.tsx
+++
@@ -1,6 +0,0 @@
-import {useSelector} from 'react-redux';
-import {selectEmployee, isAuth} from '../features/counter/employeeSlice';
-
-export default function IsAuth() {
-  return useSelector(isAuth);
-}
